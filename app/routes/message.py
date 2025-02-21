c7c219c0d45955d3a5d6fb3be129bd6b45a3f880
Tianlei Miao <nbyzmtl@gmail.com>
2025-02-21 10:45:47 +0100

    用户页面加入查看消息

--- app/routes/message.py
+++ app/routes/message.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import Blueprint, render_template, flash, redirect, url_for, request, abort
 from flask_login import current_user, login_required
 from app import db
 from app.models import Message, User, Task
@@ -38,4 +38,43 @@ def send_message(recipient_id, task_id=None):
     return render_template('send_message.html', 
                          form=form, 
                          recipient=recipient,
-                         task=task) 
+                         task=task)
+
+@message_bp.route('/user/messages')
+@login_required
+def user_messages():
+    # 使用joinedload预加载关联数据
+    sent_messages = Message.query.options(db.joinedload(Message.task))\
+        .filter_by(sender_id=current_user.id)\
+        .order_by(Message.created_at.desc())\
+        .all()
+
+    received_messages = Message.query.options(db.joinedload(Message.task))\
+        .filter_by(recipient_id=current_user.id)\
+        .order_by(Message.created_at.desc())\
+        .all()
+
+    return render_template('user_messages.html',
+                         sent_messages=sent_messages,
+                         received_messages=received_messages)
+
+@message_bp.route('/send_message/<int:recipient_id>/<int:task_id>', methods=['POST'])
+@login_required
+def send_task_message(recipient_id, task_id):
+    task = Task.query.get_or_404(task_id)
+    if current_user.id not in [task.user_id, task.executor_id]:
+        abort(403)
+    
+    form = MessageForm()
+    if form.validate_on_submit():
+        message = Message(
+            sender_id=current_user.id,
+            recipient_id=recipient_id,
+            content=form.content.data,
+            task_id=task_id
+        )
+        db.session.add(message)
+        db.session.commit()
+        flash('消息已发送')
+    
+    return redirect(url_for('task.task_conversation', task_id=task_id)) 
