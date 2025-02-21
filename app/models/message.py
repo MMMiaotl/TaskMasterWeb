fd3e84e10b1c205adca3b68db75d1bde70320e7d
Tianlei Miao <nbyzmtl@gmail.com>
2025-02-21 10:14:31 +0100

    发消息页面错误修复

--- app/models/message.py
+++ app/models/message.py
@@ -8,7 +8,7 @@ class Message(db.Model):
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
     is_read = db.Column(db.Boolean, default=False)
 
