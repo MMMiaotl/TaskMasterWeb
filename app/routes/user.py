c7d04d6391cb0d5e8ec464f0829598f3d5e06f18
Tianlei Miao <nbyzmtl@gmail.com>
2025-02-21 16:30:47 +0100

    加入上传头像的功能(未完)

--- app/routes/user.py
+++ app/routes/user.py
@@ -3,6 +3,9 @@ from flask_login import current_user, login_required
 from app import db
 from app.forms import EditProfileForm
 from app.models import User, Review
+from werkzeug.utils import secure_filename
+import os
+from config import Config
 
 user_bp = Blueprint('user', __name__)
 
@@ -43,4 +46,30 @@ def edit_profile():
         form.bio.data = current_user.bio
         form.location.data = current_user.location
         form.website.data = current_user.website
-    return render_template('edit_profile.html', form=form) 
+    return render_template('edit_profile.html', form=form)
+
+@user_bp.route('/user/<username>/edit_avatar', methods=['GET', 'POST'])
+@login_required
+def edit_avatar(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if user.id != current_user.id:
+        flash('你没有权限编辑此用户的头像。', 'error')
+        return redirect(url_for('user.user_profile', username=username))
+    
+    if request.method == 'POST':
+        file = request.files.get('avatar')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(Config.UPLOAD_FOLDER, filename)
+            file.save(filepath)
+            user.avatar_url = url_for('static', filename=f'uploads/{filename}')
+            db.session.commit()
+            flash('头像更新成功！', 'success')
+            return redirect(url_for('user.user_profile', username=username))
+        else:
+            flash('文件类型不支持，请上传图片文件。', 'error')
+    
+    return render_template('edit_avatar.html', user=user)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'} 
